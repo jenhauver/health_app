3227ff223392b4180d01f319d47ccb0f94f58396
Jennifer Hauver <jenhauver@gmail.com>
2013-06-08 16:08:51 -0700

    from_rails_project

--- rails_projects/health_app/config/initializers/secret_token.rb
+++ rails_projects/health_app/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-HealthApp::Application.config.secret_token = 'f4fea9583d261c1a0df1168bbfc4e3bf3a7c050f5d1b0674ba4422e019ffb42820f6c61f9a9dfe6be9d9e439079c4139a13fb3f1afeb0e1f52f1aa2c0a3f16bd'
+HealthApp::Application.config.secret_token = '9e510d9c99fe54e4d9b2a8a60ee80cb5959a79942afc994ccb7ed4979aa0de1d0a66786907f7d5a6992667dc15f10d88b68f330d4c341a88f9d0fcdb2c809625'
