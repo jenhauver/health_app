44cb07f632f477d04b9143ce945dc01b1b1e1013
Jenhauver <jenhauver@gmail.com>
2013-06-19 13:25:07 -0700

    Health App First Commit on Linux

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HealthApp::Application.config.secret_token = '531e203c073d0aa519f883c0c27244393610bb8e05657e0bb84aeff8f13b31e6dad6ac17acb552891abe70e42624ba1a6b351a876272914bceaed75d72efecf5'
